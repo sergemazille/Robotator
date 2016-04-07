3f8ee3a31ff5dc66b128a36f9c8ac3bcfa187ef5
Serge <serge.mazille@gmail.com>
2016-04-07 08:45:55 +0200

    removed : codes tests reseau

--- dev/js/services/Robotator.js
+++ dev/js/services/Robotator.js
@@ -23,7 +23,6 @@ class Robotator {
     }
 
     getIPFromAppWeb(cbFunction) {
-        console.log("Start getIpFromAppWeb");
 
         let serverPort = this.PORT;
 
@@ -66,6 +65,7 @@ class Robotator {
                         alert(msg);
                     } else {
 
+                        // Debugging
                         jxcore("displayMsg").register(function(msg){
                             console.log(msg);
                         });
@@ -77,105 +77,11 @@ class Robotator {
                             },
                             function (serverIp) {
                                 if (applicationNotLaunched) {
-                                    console.log(serverIp);
                                     localStorage.setItem("lastIp", serverIp); // Enregistre l'IP utilisée pour accélérer la prochaine connection
                                     applicationNotLaunched = false; // Empêche de lancer plusieurs fois l'application
                                     cbFunction(`http://${serverIp}:${serverPort}`);
                                 }
                             });
-                    }
-                });
-            });
-        }, 5);
-    }
-
-    isInAutonomousMode(cbFunction) {
-        console.log("Start isInAutonomousMode");
-
-        let autonomousModeIP = this.AUTONOMOUS_MODE_IP;
-        let port = this.PORT;
-
-        let p = new Ping(); // testable uniquement quand Cordova est chargé sur l'application mobile
-        p.ping(autonomousModeIP,
-            function success(result) {
-                let status = result[0].status;
-
-                // Si le status est 'timeout', c'est que Robotator n'est pas en mode autonome
-                if ('timeout' === status) {
-                    return false;
-                } else if ('success' === status) {
-                    // Sinon le serveur est bien en mode autonome et on connaît son IP...
-                    console.log(`IP from 'Ping 10.0.0.1' : http://${lastIpUsed}:${port}`);
-                    cbFunction(`http://${autonomousModeIP}:${port}`);
-                }
-            },
-            function error() {
-                console.log(`Erreur de ping à l'adresse IP : ${autonomousModeIP}`);
-            });
-    }
-
-    tryLastIpUsed(cbFunction) {
-        /*console.log("Start tryLastIpUsed");
-
-         let port = this.PORT;
-         let lastIpUsed = localStorage.getItem("lastIpUsed");
-
-         console.log(`http://${lastIpUsed}:${port}`);
-
-         if (null != lastIpUsed) {
-         let p = new Ping(); // testable uniquement quand Cordova est chargé sur l'application mobile
-
-         p.ping(lastIpUsed,
-         function success(result) {
-         let status = result[0].status;
-
-         // Si le status est 'timeout', c'est que Robotator n'est pas en mode autonome
-         if ('timeout' === status) {
-         return false; // lancer la méthode de recherche suivante
-         } else if ('success' === status) {
-
-         console.log(`IP from 'lastIpUsed' : http://${lastIpUsed}:${port}`);
-         cbFunction(`http://${lastIpUsed}:${port}`);
-         }
-         },
-         function error() {
-         console.log(`Erreur de ping à l'adresse IP : ${lastIpUsed}`);
-         });
-         }*/
-    }
-
-    getIPFromAppWebService(cbFunction) {
-        console.log("Start getIPFromAppWebService");
-
-        let port = this.PORT;
-
-        //JXCore stuff...
-        let inter = setInterval(function () {
-
-            if (typeof jxcore == 'undefined') return;
-            clearInterval(inter);
-            jxcore.isReady(function () {
-
-                jxcore('app.js').loadMainFile(function (ret, err) {
-                    if (err) {
-                        let msg;
-                        if (!err || err.replace)
-                            msg = err;
-                        else
-                            msg = JSON && JSON.stringify ? JSON.stringify(err) : err;
-                        alert(msg);
-                    } else {
-
-                        // Système de messages (debugging)
-                        jxcore('msg').register(function (msg) {
-                            console.log(`Message from nodeServices : ${msg}`);
-                        });
-
-                        // Le module Polo de JXCore renvoie l'adresse IP du serveur
-                        jxcore('getIPFromAppWebService').call(function (serverIP) {
-                            console.log(`IP from 'Polo' : http://${serverIP}:${port}`);
-                            cbFunction(`http://${serverIP}:${port}`);
-                        });
                     }
                 });
             });
