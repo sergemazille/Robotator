3f9bc920aace8994efcb8b68163901e61519fff2
Serge <serge.mazille@gmail.com>
2016-04-08 09:00:38 +0200

    fichiers tests generes : untracked

--- tests/AppMobile/www/js/unitTests/testClient.js
+++
@@ -1,7 +0,0 @@
-// Tests
-QUnit.test( "Objet Client défini", function( assert ) {
-    assert.notEqual(Client.isMobileDevice, undefined || null, `(Client.isMobileDevice)   =>   ${Client.isMobileDevice}`);
-    assert.notEqual(Client.isMobileApp,  undefined, `(Client.isMobileApp)   =>   ${Client.isMobileApp}`);
-    assert.notEqual(Client.isWebApp, undefined, `(Client.isWebApp)   =>   ${Client.isWebApp}`);
-    assert.notEqual(Client.language, undefined, `(Client.language)   =>   ${Client.language}`);
-});
--- tests/AppWeb/public/js/unitTests/general.js
+++
@@ -1,5 +0,0 @@
-(function hideRobotatorInterface(){
-    // n'affiche pas les éléments de l'application en elle-même
-    document.getElementsByClassName("vex")[0].style.display = 'none';
-    document.getElementById("interface").style.display = 'none';
-})();
